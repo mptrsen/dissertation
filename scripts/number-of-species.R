971b08572738366961567e4bff8f3a530fc165f5
Malte Petersen <mptrsen@uni-bonn.de>
2018-07-12 13:14:47 +0200

    Update biodiversity figure, add IUCN data source

--- scripts/number-of-species.R
+++ scripts/number-of-species.R
@@ -7,9 +7,16 @@ dt <- tibble(
 	n = c(55000, 750000, 5000, 67000, 280000, 270000 )
 )
 
+dt <- tibble(
+	group = c("insects", "vertebrates", "other invertebrates", "plants, red, green algae", "lichen, fungi, brown algae", "foo"),
+	n = c(1000000, 66178, 305250, 307674, 51623, 0)
+)
+
+dt <- read_tsv('tables/number-of-species.csv')
+
 total <- sum(dt$n)
 
-dt <- dt %>% mutate(perc = percent(dt$n/total), label = paste0(group, ' (', n, ')'))
+dt <- dt %>% group_by(group) %>%  summarise(n = sum(n)) %>% mutate(perc = percent(n/total), label = paste0(group, ' (', n, ')'))
 
 blank_theme <- theme_minimal() +
 	theme(axis.title = element_blank(), axis.title.y = element_blank(),
@@ -21,9 +28,10 @@ blank_theme <- theme_minimal() +
 plt <- ggplot(dt, aes(1, n, fill = group)) + geom_bar(stat = 'identity', width = 1)
 plt <- plt + scale_fill_brewer(palette = 'BrBG')
 plt <- plt + coord_polar('y')
+plt <- plt + xlim(c(0,1.5))
 plt <- plt + blank_theme
 plt <- plt + geom_text(aes(y = n, label = label))
 print(plt)
 
 # don't overwrite by accident!
-#save_plot('figures/number-of-species.svg', plot = plt)
+save_plot('figures/number-of-species.svg', plot = plt)
